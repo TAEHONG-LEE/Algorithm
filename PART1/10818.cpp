b6b6b39a34c71bf15015c6c8c65d9c7c5a70af48
TAEHONG-LEE <2t_hong@naver.com>
2024-02-23 13:16:42 +0900

    Update Part1 10818

---
+++ PART1/10818.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    int min = 1000000;
+    int max = -1000000;
+    int digit[1000000];
+
+    cin >> num;
+    for(int i = 0; i < num; i++){
+        cin >> digit[i];
+        if(digit[i] > max){
+            max = digit[i];
+        } 
+        if(digit[i] < min) {
+            min = digit[i];
+        }
+    }
+
+    cout << min << " " << max << endl;
+
+    return 0;
+}
